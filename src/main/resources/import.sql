d82aea5e54b297305de40e2cf0399eb962247188
andre_zktt <andrezicatti@gmail.com>
2025-02-13 18:31:30 -0300

    :sparkles: feat:  entities - course

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,10 +7,10 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
-
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
-
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML5 e CSS3', 'https://pix4free.org/assets/library/2024-11-05/originals/training-course.jpg', 'https://www.taalhammer.com/wp-content/uploads/2024/04/Effective-Communication-Strategies-for-Language-Learners-768x495.jpeg');
