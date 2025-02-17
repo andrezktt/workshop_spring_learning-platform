ef441214852a52a20a8e23f8782d27151bf44a10
andre_zktt <andrezicatti@gmail.com>
2025-02-17 11:51:13 -0300

    :card_file_box: raw: seeding - enrollment

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -25,3 +25,6 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) 
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'História da internet e estrutura HTML', 1, 'https://pix4free.org/assets/library/2024-11-05/originals/training-course.jpg', 1, null);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Diferenças entre HTML, CSS e Javascript', 2, 'https://pix4free.org/assets/library/2024-11-05/originals/training-course.jpg', 1, 1);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Instalando as ferramentas necessárias', 3, 'https://pix4free.org/assets/library/2024-11-05/originals/training-course.jpg', 1, 2);
+
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1,TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1,TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false);
